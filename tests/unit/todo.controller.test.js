9a63945eaa5931719acb057f4437cfc1ad8d1bef
Danilo Titato <danilotitato@gmail.com>
2021-05-05 21:03:24 -0300

    Created mongoose model

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,7 +1,14 @@
 const todoController = require('../../src/controllers/todo.controller')
+const todoModel = require('../../src/models/todo.model')
+
+todoModel.create = jest.fn()
 
 describe('todoController.createTodo', () => {
     it('should have a createTodo function', () => {
         expect(typeof todoController.createTodo).toBe('function')
+    })
+    it('should call todoModel.create', () => {
+        todoController.createTodo()
+        expect(todoModel.create).toBeCalled()
     })
 })
